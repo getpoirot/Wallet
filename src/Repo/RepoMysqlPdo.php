09fb5fea7e6b0bd6d38104746f4d59ccf238018a
mostafa <sorena.551@gmail.com>
2017-08-07 05:13:48 -0400

    change  refrence of EntityWallet implement insert method add krumo package to composer.json add index.php file for testing

--- src/Repo/RepoMysqlPdo.php
+++ src/Repo/RepoMysqlPdo.php
@@ -1,7 +1,8 @@
 <?php
-namespace Poirot\Wallet;
+namespace Poirot\Wallet\Repo;
 
 use Poirot\Wallet\Interfaces\iRepoWallet;
+use Poirot\Wallet\Entity\EntityWallet;
 
 
 class RepoMysqlPdo
@@ -43,7 +44,47 @@ class RepoMysqlPdo
      */
     function insert(EntityWallet $entityWallet)
     {
-        // TODO: Implement insert() method.
+        $prevSql="SELECT 
+    last_total
+FROM
+    `transactions`
+WHERE 
+    uid=$entityWallet->getUid
+AND 
+    wallet_type=$entityWallet->getWalletType
+
+ORDER BY id DESC
+LIMIT 1;;";
+
+        $stmt = $this->conn->prepare($prevSql);
+        $stmt->execute();
+        $stmt->setFetchMode(\PDO::FETCH_ASSOC);
+        $result = $stmt->fetch();
+        if (!$result){
+            $result=0;
+        }
+        $result=$result+$entityWallet->getLastTotal();
+        var_dump($entityWallet);
+
+        $sql="INSERT INTO transactions (
+                 uid, wallet_type, amount, source, data_created,last_total
+                )
+                VALUES ('{$entityWallet->getUid()}'
+                , '{$entityWallet->getWalletType()}'
+                , '{$entityWallet->getAmount()}'
+                , '{$entityWallet->getSource()}'
+                , '{$entityWallet->getDateCreated()->format('Y/m/d H:i:s')}'
+                , '{$result}'
+                )
+                
+        ";
+        var_dump(2);
+        $this->conn->exec($sql);
+
+
+
+
+
     }
 
     /**
